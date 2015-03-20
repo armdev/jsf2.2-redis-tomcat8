803a064ad8295bbebd3c5018491e9de931525332
Home <Home@armdev.ubuntu.com>
2015-03-21 01:25:19 +0400

    commit

---
+++ jsfredis/src/main/java/com/project/redis/Person.java
@@ -0,0 +1,50 @@
+package com.project.redis;
+
+import java.io.Serializable;
+import java.util.StringTokenizer;
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+import javax.validation.constraints.Size;
+
+public class Person implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String name;
+
+    private int age;
+
+    public Person() {
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return name + ", " + age;
+    }
+
+    public static Person fromString(String string) {
+        StringTokenizer tokens = new StringTokenizer(string, ",");
+        return new Person(tokens.nextToken(), Integer.parseInt(tokens.nextToken().trim()));
+    }
+}
---
+++ jsfredis/src/main/java/com/project/redis/PersonBean.java
@@ -0,0 +1,83 @@
+package com.project.redis;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
+
+import redis.clients.jedis.Jedis;
+
+@ManagedBean(name = "personBean")
+@SessionScoped
+public class PersonBean implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Person person = new Person();
+
+    private Jedis jedis;
+
+    private Set<String> set = new HashSet<String>();
+
+    public PersonBean() {
+    }
+
+    @PostConstruct
+    private void initDB() {
+//         Start embedded Redis
+        jedis = new Jedis("localhost", 6379);
+    }
+
+    @PreDestroy
+    private void stopDB() {
+        jedis.shutdown();
+    }
+
+    public void createPerson() {
+        jedis.set(person.getName(), new Person(person.getName(), person.getAge()).toString());
+        Person retrunBack = Person.fromString(jedis.get(person.getName()));
+        //System.out.println("retrunBack " + retrunBack.getName());
+        set.add(person.getName());
+    }
+
+    public List<Person> getPersons() {
+        List<Person> persons = new ArrayList<Person>();
+        for (String key : set) {
+            persons.add(Person.fromString(jedis.get(key)));
+        }
+        //System.out.println("persons " + persons.size());
+
+        return persons;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public Jedis getJedis() {
+        return jedis;
+    }
+
+    public void setJedis(Jedis jedis) {
+        this.jedis = jedis;
+    }
+
+    public Set<String> getSet() {
+        return set;
+    }
+
+    public void setSet(Set<String> set) {
+        this.set = set;
+    }
+
+}
